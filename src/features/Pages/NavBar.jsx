05dfe1c490f1e7e53f5e0da486972c811f97106a
WheelShare-Project <wheelsharegroup@gmail.com>
2025-04-23 20:43:04 +0300

    trying

--- src/features/Pages/NavBar.jsx
+++ src/features/Pages/NavBar.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { FaShekelSign, FaInfoCircle, FaHeadset, FaUser } from "react-icons/fa";
 export default function NavBar() {
   return (
     <>
       <div className="sidebar">
-        <Link to="/pricing" className="sidebar-item">
+        <a href="/pricing" className="sidebar-item">
           <FaShekelSign className="icon" />
           <span>מחירון</span>
-        </Link>
-
-        <Link to="/guide" className="sidebar-item">
+        </a>
+        <a href="/guide" className="sidebar-item">
           <FaInfoCircle className="icon" />
           <span>קצת עלינו</span>
-        </Link>
-
-        <Link to="/contact" className="sidebar-item">
+        </a>
+        <a href="/contact" className="sidebar-item">
           <FaHeadset className="icon" />
           <span>צור קשר</span>
-        </Link>
+        </a>
+      
       </div>
     </>
   );
